79c3144a3391167c92bd84ab31144d6063a63803
Hassan Alekri <alekrih@tcd.ie>
2025-04-03 17:15:51 +0100

    Added num_classes parameter for weighting

--- options/base_options.py
+++ options/base_options.py
@@ -41,6 +41,8 @@ class BaseOptions():
         parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
         parser.add_argument('--suffix', default='', type=str, help='customized suffix: opt.name = opt.name + suffix: e.g., {model}_{netG}_size{loadSize}')
         parser.add_argument('--delr_freq', type=int, default=20, help='frequency of changing lr')
+        parser.add_argument('--num_classes', type=int, default=4, help='Number of classes in dataset (RR, RF, FR, FF)')
+
 
         
         self.initialized = True
